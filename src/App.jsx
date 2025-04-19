a76bab53f058450dfb6ce7bf4417105446d472ec
Subhoniddin <subhoniddin571@gmail.com>
2025-04-19 22:44:15 +0500

    faqat font qoshdim

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 
 import './App.css'
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
 function App() {
